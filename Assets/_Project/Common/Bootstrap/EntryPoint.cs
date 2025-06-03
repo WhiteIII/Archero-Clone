54c72378a4ed0fd739389b24ae55efb72ae543de
alexandrKozhanyazov <whiteminacraft90@gmail.com>
2025-06-03 22:41:37 +0500

    removed new input system

--- Assets/_Project/Common/Bootstrap/EntryPoint.cs
+++ Assets/_Project/Common/Bootstrap/EntryPoint.cs
@@ -1,16 +1,9 @@
 using UnityEngine;
 
-public class EntryPoint : MonoBehaviour
+namespace Project.Bootstrap
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public class EntryPoint
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+    
     }
 }
